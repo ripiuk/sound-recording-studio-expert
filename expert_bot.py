62176f82a934df4f9c73595aadafe1e3dc935947
oleksandrr <oleksandrr@fornova.net>
2018-05-20 11:14:29 +0300

    Added warning messages during the quiz

---
+++ expert_bot.py
@@ -0,0 +1,80 @@
+import json
+import logging
+
+import requests
+
+
+class ExpertBotHandler:
+
+    def __init__(self, token: str):
+        self.token = token
+        self.api_url = "https://api.telegram.org/bot{}/".format(token)
+        self.log = logging.getLogger(f'Bot.{type(self).__name__}')
+
+    def get_updates(self, offset: int=None, timeout: int=30) -> list:
+        """Make a GET request to get all the updates
+
+        :param offset: Identifier of the first update to be returned.
+        Must be greater by one than the highest among the identifiers of previously received updates.
+        By default, updates starting with the earliest unconfirmed update are returned.
+        :param timeout: Timeout in seconds for long polling. Defaults to 0, i.e. usual short polling.
+        Should be positive, short polling should be used for testing purposes only.
+        :return: a list of update results
+        """
+        method = 'getUpdates'
+        params = {'timeout': timeout, 'offset': offset}
+        resp = requests.get(self.api_url + method, params)
+        result = resp.json()['result']
+        self.log.debug(f'Got updates: {result}')
+        return result
+
+    def send_message(self, chat_id: int or str, text: str,
+                     reply_markup: str=None, parse_mode: str='Markdown') -> requests.models.Response():
+        """
+        :param chat_id: Unique identifier for the target chat or username of the target channel
+        :param text: Text of the message to be sent
+        :param reply_markup: Additional interface options. A JSON-serialized object for an inline keyboard,
+        custom reply keyboard, instructions to remove reply keyboard or to force a reply from the user.
+        :param parse_mode: Send Markdown or HTML, if you want Telegram apps to show bold, italic,
+        fixed-width text or inline URLs in your bot's message.
+        """
+        method = 'sendMessage'
+        params = {'chat_id': chat_id, 'text': text, 'parse_mode': parse_mode}
+        if reply_markup:
+            params = {**params, 'reply_markup': reply_markup}
+        resp = requests.post(self.api_url + method, params)
+        self.log.debug(f'Message delivery status: {resp.status_code}')
+        return resp
+
+    def get_last_update(self) -> tuple:
+        """Get last update and parse a response
+
+        :return: tuple(last_update_id, last_chat_text, last_chat_id)
+        """
+        get_result = self.get_updates()
+        if not get_result:
+            return None, None, None
+        last_update = get_result[-1]
+        return self.parse_update_message(last_update)
+
+    @staticmethod
+    def build_keyboard(items: list) -> str:
+        """
+        :param items: a list of button names
+        :return: a JSON-serialized object for a custom reply keyboard
+        """
+        keyboard = [[item] for item in items]
+        reply_markup = {"keyboard": keyboard, "one_time_keyboard": True, "resize_keyboard": True}
+        return json.dumps(reply_markup)
+
+    @staticmethod
+    def remove_keyboards() -> str:
+        reply_markup = {"remove_keyboard": True}
+        return json.dumps(reply_markup)
+
+    @staticmethod
+    def parse_update_message(message: dict) -> tuple:
+        last_update_id = message['update_id']
+        last_chat_text = message['message']['text']
+        last_chat_id = message['message']['chat']['id']
+        return last_update_id, last_chat_text, last_chat_id
---
+++ run_bot.py
@@ -0,0 +1,172 @@
+import misc
+from expert_bot import ExpertBotHandler
+from experts import AudioInterface, Soundproofing
+
+EQUIPMENTS = [{
+        'Аудіо інтерфейс': AudioInterface,
+        'Шумоізоляція': Soundproofing,
+        'Мікрофон': None,
+        'Студійні монітори': None,
+        'Мікшерський пульт': None
+    }, {
+        'Audio interface': AudioInterface,
+        'Soundproofing': Soundproofing,
+        'Microphone': None,
+        'Studio monitor': None,
+        'Mixing console': None
+    }]
+LIST_OF_ANSWERS = [['Ні', 'Швидше за все - ні', 'Не знаю', 'Швидше за все - так', 'Так'],
+                   ['No', 'Probably no', 'Don\'t know', 'Probably', 'Yes']]
+START_TEXT = ['Щоб обрати категорію введіть команду: /menu', 'To select a category, enter the command: /menu']
+STOP_TEXT = ['_Щоб вийти з данного опитування введіть команду:_ /stop',
+             '_To exit this survey, enter the following command:_ /stop']
+MENU_TEXT = ['Оберіть звукозаписуюче обладнання', 'Choose a type of sound recording equipment']
+HELP_TEXT = [['MusicStudioExpertBot допоможе вам з укомплектуванням домашньої студії звукозапису. '
+              'Вам лише необхідно відповісти на поставленні питання.',
+              'Щоб обрати категорію введіть команду: /menu',
+              'Також данний бот підтримує наступні команди: /start /help /settings',
+              '\n\n[GitHub source](https://github.com/ripiuk)'],
+             ['MusicStudioExpertBot is a bot, that can help you to provide a home sound recording studio.',
+              'To choose a category from menu - use this command: /menu',
+              'Also, this bot supports the following commands: /start /help /settings',
+              '\n\n[GitHub source](https://github.com/ripiuk)']]
+SETTINGS_TEXT = ['Оберіть мову (лише для навігації)',
+                 'Here you can choose a language that you prefer (for navigation only)']
+NOT_AVAILABLE_TEXT = ['Ви не можете використовувати команди під час опитування.',
+                      'All commands are not available during the quiz.']
+QUESTION_NUMBER_PREFIX = ['Поточне питання: ', 'Current question: ']
+DONE_MESSAGE = ['Готово', 'Done']
+LANGUAGES = {'🇺🇦UA': 0, '🇺🇸US': 1}
+DEFAULT_LANGUAGE_ID = 0  # UA
+
+
+def send_question(expert_bot: ExpertBotHandler, last_chat_id: int, question_text: str,
+                  question_number: int, number_of_questions: int, current_language_id: int) -> None:
+    """Send current question from quiz to user
+    """
+    keyboard = expert_bot.build_keyboard(LIST_OF_ANSWERS[current_language_id])
+    expert_bot.send_message(chat_id=last_chat_id,
+                            text=f'{QUESTION_NUMBER_PREFIX[current_language_id]}'
+                                 f'({question_number + 1}/{number_of_questions})\n'
+                                 f'*{question_text}*\n\n'
+                                 f'{STOP_TEXT[current_language_id]}',
+                            reply_markup=keyboard)
+
+
+def main():
+    my_token = misc.token
+    expert_bot = ExpertBotHandler(my_token)
+    chat_language_for_user = dict()  # chat_id: language_id
+    current_step_for_user = dict()  # e.g chat_id: {expert_class: expert, current_step: 0}
+    new_offset = None
+
+    while True:
+        expert_bot.get_updates(new_offset)
+        for update in expert_bot.get_updates():
+            last_update_id, last_chat_text, last_chat_id = expert_bot.parse_update_message(update)
+            if not last_update_id:
+                # Update response is empty
+                continue
+            current_language_id = chat_language_for_user.get(last_chat_id, DEFAULT_LANGUAGE_ID)
+            is_current_user_in_quiz = current_step_for_user.get(last_chat_id, dict()).get(
+                'current_step') or current_step_for_user.get(last_chat_id, dict()).get('current_step') == 0
+
+            if is_current_user_in_quiz:
+                if last_chat_text in LIST_OF_ANSWERS[current_language_id]:
+                    question_number = current_step_for_user[last_chat_id]['current_step']
+                    expert_system = current_step_for_user[last_chat_id]['expert_class']
+                    number_of_questions = len(expert_system.questions)
+
+                    if last_chat_text == LIST_OF_ANSWERS[current_language_id][0]:
+                        # No
+                        expert_system.handle_answer(question_number, 0)
+                    elif last_chat_text == LIST_OF_ANSWERS[current_language_id][1]:
+                        # Probably no
+                        expert_system.handle_answer(question_number, 1)
+                    elif last_chat_text == LIST_OF_ANSWERS[current_language_id][2]:
+                        # Do not know
+                        pass
+                    elif last_chat_text == LIST_OF_ANSWERS[current_language_id][3]:
+                        # Probably
+                        expert_system.handle_answer(question_number, 3)
+                    elif last_chat_text == LIST_OF_ANSWERS[current_language_id][4]:
+                        # Yes
+                        expert_system.handle_answer(question_number, 4)
+
+                    question_number += 1
+
+                    if question_number >= number_of_questions:
+                        # Send results
+                        result = expert_system.get_result()
+                        expert_bot.send_message(
+                            last_chat_id, f"Result:\nProducer: {result.get('producer')}\n"
+                                          f"Model: {result.get('model')}\n",
+                            reply_markup=expert_bot.remove_keyboards())
+                        del current_step_for_user[last_chat_id]
+                    else:
+                        # Send next question
+                        current_step_for_user[last_chat_id]['current_step'] = question_number
+                        question_text = expert_system.questions[question_number]
+
+                        send_question(expert_bot, last_chat_id, question_text,
+                                      question_number, number_of_questions, current_language_id)
+
+                elif last_chat_text == '/stop':
+                    # Stop this quiz
+                    del current_step_for_user[last_chat_id]
+                    expert_bot.send_message(chat_id=last_chat_id,
+                                            text=DONE_MESSAGE[current_language_id],
+                                            reply_markup=expert_bot.remove_keyboards())
+
+                elif last_chat_text.startswith('/'):
+                    expert_bot.send_message(chat_id=last_chat_id, text=NOT_AVAILABLE_TEXT[current_language_id])
+                    expert_system = current_step_for_user[last_chat_id]['expert_class']
+                    question_number = current_step_for_user[last_chat_id]['current_step']
+                    question_text = expert_system.questions[question_number]
+                    number_of_questions = len(expert_system.questions)
+
+                    send_question(expert_bot, last_chat_id, question_text,
+                                  question_number, number_of_questions, current_language_id)
+
+            elif last_chat_text == '/start':
+                expert_bot.send_message(last_chat_id, START_TEXT[current_language_id],
+                                        reply_markup=expert_bot.remove_keyboards())
+
+            elif last_chat_text == '/menu':
+                keyboard = expert_bot.build_keyboard(list(EQUIPMENTS[current_language_id].keys()))
+                expert_bot.send_message(last_chat_id, MENU_TEXT[current_language_id], reply_markup=keyboard)
+
+            elif last_chat_text == '/help':
+                expert_bot.send_message(
+                    chat_id=last_chat_id, text='\n'.join(HELP_TEXT[current_language_id]),
+                    reply_markup=expert_bot.remove_keyboards())
+
+            elif last_chat_text == '/settings':
+                keyboard = expert_bot.build_keyboard(list(LANGUAGES.keys()))
+                expert_bot.send_message(last_chat_id, SETTINGS_TEXT[current_language_id], reply_markup=keyboard)
+
+            elif last_chat_text in LANGUAGES:
+                chat_language_for_user[last_chat_id] = LANGUAGES.get(last_chat_text, DEFAULT_LANGUAGE_ID)
+                expert_bot.send_message(last_chat_id, DONE_MESSAGE[chat_language_for_user[last_chat_id]],
+                                        reply_markup=expert_bot.remove_keyboards())
+
+            elif last_chat_text in EQUIPMENTS[current_language_id]:
+                chosen_class = EQUIPMENTS[current_language_id][last_chat_text]
+                expert_system = chosen_class()
+                current_step_for_user[last_chat_id] = dict(expert_class=expert_system, current_step=0)
+
+                # Send first question to user
+                number_of_questions = len(expert_system.questions)
+                question_number = 0
+                question_text = expert_system.questions[question_number]
+                send_question(expert_bot, last_chat_id, question_text,
+                              question_number, number_of_questions, current_language_id)
+
+            new_offset = last_update_id + 1
+
+
+if __name__ == '__main__':
+    try:
+        main()
+    except KeyboardInterrupt:
+        exit()
